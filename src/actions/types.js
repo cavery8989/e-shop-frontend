79b8ceba64ce9b68e58cbe3c3ae73e7fb85a0648
Chris Avery <Chris@Anonymouss-MacBook-Air.local>
2016-08-02 12:27:01 +0100

    fixed type

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,6 +4,6 @@ export const ADD_ITEM = 'ADD_ITEM';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
 export const EMPTY_BASKET = 'EMPTY_BASKET';
 export const TOTAL_ITEMS = 'TOTAL_ITEMS';
-export const PASS_CART = 'PASS_ITEM';
+export const PASS_CART = 'PASS_CART';
 
 
