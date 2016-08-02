4469e0bc5cb7eb08de266c1d6410a9ccd5a3bc70
Chris Avery <Chris@Anonymouss-MacBook-Air.local>
2016-08-02 12:25:45 +0100

    defined action constants

---
+++ src/actions/types.js
@@ -0,0 +1,9 @@
+// Action contstants;
+
+export const ADD_ITEM = 'ADD_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const EMPTY_BASKET = 'EMPTY_BASKET';
+export const TOTAL_ITEMS = 'TOTAL_ITEMS';
+export const PASS_CART = 'PASS_ITEM';
+
+
