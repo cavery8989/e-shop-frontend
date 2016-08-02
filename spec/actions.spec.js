92f22c2168544eaaddfd31ca01c946e07c25d601
Apswak <williamkreitz@gmail.com>
2016-08-02 12:31:48 +0100

    Added all functions

--- spec/actions.spec.js
+++ spec/actions.spec.js
@@ -1,4 +1,3 @@
-
 import { expect } from 'chai';
 import actions from '../src/actions/actions';
 
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,12 +1,38 @@
 let actions = {};
 
+import * as types from './types';
+
 actions.addItem = item => {
-  return{
-    type: 'ADD_ITEM',
+  return {
+    type: types.ADD_ITEM,
     item: item
   }
 };
 
+actions.removeItem = item => {
+  return {
+    type: types.REMOVE_ITEM,
+    item: item
+  }
+};
+
+actions.empty = () => {
+  return {
+    type: types.EMPTY_BASKET
+  }
+};
+
+actions.totalItems = () => {
+  return {
+    type: types.TOTAL_ITEMS
+  }
+};
+
+actions.passCart = () => {
+  return {
+    type: types.PASS_CART
+  }
+}
+
 
 export default actions;
-
