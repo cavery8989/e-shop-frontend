50a42712c4e007e95ed16b820021743b1a74c03a
Apswak <williamkreitz@gmail.com>
2016-08-02 17:15:37 +0100

    Added ADD_ITEM & REMOVE_ITEM to reducer & tests.

--- spec/reducer.spec.js
+++ spec/reducer.spec.js
@@ -1,10 +1,25 @@
 import { expect } from 'chai';
 import appReducer from '../src/reducer/reducer';
 
-describe('#appReducer', function () {
-  it('is a function', function () {
+describe('#appReducer', () => {
+  it('is a function', () => {
     expect(appReducer).to.be.a('function');
   });
 
-});
+  var initialState = { basket: ['hi'] }
 
+  it('should return initialState if action is not recognized', () => {
+    var testAction = {type: 'nonExisting', item: 'cheese'}
+    expect(appReducer(initialState, testAction) ).to.eql(initialState);
+  });
+
+  it('should return initialState if theres no action', () => {
+    expect(appReducer(initialState) ).to.eql(initialState);
+  });
+
+  // Add item
+  it('should add the item passed into ADD_ITEM', () => {
+    var testAction = {type: 'ADD_ITEM', item: 'cheese'}
+    expect(appReducer(initialState, testAction)).eql({basket: ['hi', 'cheese']});
+  });
+});
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,16 +2,27 @@ import * as types from '../actions/types';
 
 const appReducer = (initialState, action) => {
   initialState = initialState || {basket:[]};
-
   let newState;
 
+  action = action || {type: undefined};
+
   switch(action.type){
-    case types.ADD_ITEM :
+    case types.ADD_ITEM:
       newState = Object.assign({},initialState, { basket: initialState.basket.concat(action.item)});
       break;
-  }
+    case types.REMOVE_ITEM :
+      var newBasket = initialState.basket.slice();
+      var index = newBasket.indexOf(action.item);
+      newBasket.splice(index, 1);
+      newState = Object.assign({}, initialState, {
+        basket: newBasket
+      });
+      break;
 
-
-};
+    default:
+      newState = Object.assign({}, initialState);
+    }
+    return newState;
+  };
 
 export default appReducer;
