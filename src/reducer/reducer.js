3a6ee3b06b88424e6afd83e9f9af933b4338d26c
Apswak <williamkreitz@gmail.com>
2016-08-05 16:53:49 +0100

    Added books to library and structured

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -27,6 +27,27 @@ const appReducer = (initialState, action) => {
           price: 10.02
         },
         {
+          book_name: 'The Good Parts',
+          img: 'http://d.gr-assets.com/books/1328834793l/2998152.jpg',
+          author: 'Douglas Crockford',
+          publisher: "'O'Reilly",
+          price: 24.99
+        },
+        {
+          book_name: 'Patterns',
+          img: 'http://d.gr-assets.com/books/1344668503l/9422683.jpg',
+          author: 'Stoyan Stefanov',
+          publisher: "'O'Reilly",
+          price: 19.99
+        },
+        {
+          book_name: 'High Performance',
+          img: 'http://d.gr-assets.com/books/1328834985l/7008656.jpg',
+          author: 'Nicholas C. Zakas',
+          publisher: "'O'Reilly",
+          price: 23.49
+        },
+        {
           book_name: 'Types & Grammar',
           img: 'https://slack-imgs.com/?c=1&o1=wi333.he500&url=https%3A%2F%2Fraw.githubusercontent.com%2Fgetify%2FYou-Dont-Know-JS%2Fmaster%2Ftypes%2520%2526%2520grammar%2Fcover.jpg',
           author: 'Kyle Sympson',
@@ -46,6 +67,20 @@ const appReducer = (initialState, action) => {
           author: 'Kyle Sympson',
           publisher: "'O'Reilly",
           price: 12.11
+        },
+        {
+          book_name: 'Even Faster Websites',
+          img: 'http://d.gr-assets.com/books/1328834858l/6438581.jpg',
+          author: 'Steve Souders',
+          publisher: "'O'Reilly",
+          price: 24.99
+        },
+        {
+          book_name: 'jQuery Cookbook',
+          img: 'http://d.gr-assets.com/books/1328834858l/6438581.jpg',
+          author: 'Community Experts',
+          publisher: "'O'Reilly",
+          price: 24.20
         }
       ]
     };
