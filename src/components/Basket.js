565eff0e1949741a697dd323e64eb6cdc0bd2128
Chris Avery <Chris@Anonymouss-MacBook-Air.local>
2016-08-05 15:45:21 +0100

    Refactord basket list to show single titles with quantities

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import {Link} from 'react-router'
+import _ from 'underscore';
 
 import Actions from '../actions/actions'
 
@@ -24,11 +25,30 @@ const Basket = React.createClass({
       return mem + cv.price;
     }, 0);
 
-    var listNodes = this.props.basket.map((item, index) => {
+    var condensedList = this.props.basket.reduce((mem,cur)=>{
+      let index =  _.find(mem,(obj) => obj.book_name === cur.book_name);
+
+      if(index){
+        index.quantity += 1;
+        index.idArr.push(cur.id);
+        return mem;
+
+      }else {
+        let bookObj = {book_name: cur.book_name, quantity: 1, idArr: [cur.id]};
+        mem.push(bookObj);
+        return mem;
+      }
+    },[]);
+
+    console.log('list', condensedList);
+
+
+
+    var listNodes = condensedList.map((item, index) => {
       return (
         <li key={index}><i className="fa fa-times"
                            onClick={this.props.dispatchRemoveItem.bind(null, item.id)}
-                           aria-hidden="true"/>{item.book_name + ':  £' + item.price}</li>
+                           aria-hidden="true"/>{item.book_name + ' X' + item.quantity}</li>
       )
     });
     var sidebarVisible = this.state.sidebarVisible ? 'basket-container visible' : 'basket-container hidden';
