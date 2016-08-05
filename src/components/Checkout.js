a69781d9cdb8b8852f58a80ca2074905b9fe3f16
Chris Avery <Chris@Anonymouss-MacBook-Air.local>
2016-08-05 17:10:56 +0100

    Added itemNodes & displaying name

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,13 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router'
 
- export default React.createClass({
+import {connect} from 'react-redux';
+
+ const Checkout = React.createClass({
   render () {
+
+    let itemNodes = this.props.basket.map(item => (<li>{item.book_name}</li>));
+
     return(
       <div className="checkout-container">
         <div className="columns">
@@ -13,7 +18,14 @@ import {Link} from 'react-router'
             <h1> Price </h1>
           </div>
         </div>
+        <div className="checkout-item-list">
+          <ul>
+            {itemNodes}
+          </ul>
+        </div>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
         
         "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 
@@ -28,6 +40,26 @@ import {Link} from 'react-router'
     )
   }
 });
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    remove: function () {
+
+    },
+    add: function () {
+
+    }
+  }
+};
+
+
+const mapStateToProps = (state) =>{
+  return({
+    basket: state.basket
+  })
+};
+
+export default connect(mapStateToProps)(Checkout)
 
 
 // <Link to="/shop"><i className="fa fa-shopping-cart" aria-hidden="true"></i>  Keep shopping</Link>
