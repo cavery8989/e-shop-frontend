fd535cfc90ced0557e4cfe8a0c050ba5bbff9aba
Apswak <williamkreitz@gmail.com>
2016-08-08 14:38:06 +0100

    Stable version

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -27,27 +27,6 @@ const appReducer = (initialState, action) => {
           price: 10.02
         },
         {
-          book_name: 'The Good Parts',
-          img: 'http://d.gr-assets.com/books/1328834793l/2998152.jpg',
-          author: 'Douglas Crockford',
-          publisher: "'O'Reilly",
-          price: 24.99
-        },
-        {
-          book_name: 'Patterns',
-          img: 'http://d.gr-assets.com/books/1344668503l/9422683.jpg',
-          author: 'Stoyan Stefanov',
-          publisher: "'O'Reilly",
-          price: 19.99
-        },
-        {
-          book_name: 'High Performance',
-          img: 'http://d.gr-assets.com/books/1328834985l/7008656.jpg',
-          author: 'Nicholas C. Zakas',
-          publisher: "'O'Reilly",
-          price: 23.49
-        },
-        {
           book_name: 'Types & Grammar',
           img: 'https://slack-imgs.com/?c=1&o1=wi333.he500&url=https%3A%2F%2Fraw.githubusercontent.com%2Fgetify%2FYou-Dont-Know-JS%2Fmaster%2Ftypes%2520%2526%2520grammar%2Fcover.jpg',
           author: 'Kyle Sympson',
@@ -77,10 +56,38 @@ const appReducer = (initialState, action) => {
         },
         {
           book_name: 'jQuery Cookbook',
-          img: 'http://d.gr-assets.com/books/1328834858l/6438581.jpg',
+          img: 'https://slack-imgs.com/?c=1&url=http%3A%2F%2Fd.gr-assets.com%2Fbooks%2F1328834694l%2F7107155.jpg',
           author: 'Community Experts',
           publisher: "'O'Reilly",
           price: 24.20
+        },
+        {
+          book_name: 'The Good Parts',
+          img: 'http://d.gr-assets.com/books/1328834793l/2998152.jpg',
+          author: 'Douglas Crockford',
+          publisher: "'O'Reilly",
+          price: 24.99
+        },
+        {
+          book_name: 'Patterns',
+          img: 'http://d.gr-assets.com/books/1344668503l/9422683.jpg',
+          author: 'Stoyan Stefanov',
+          publisher: "'O'Reilly",
+          price: 19.99
+        },
+        {
+          book_name: 'High Performance',
+          img: 'http://d.gr-assets.com/books/1328834985l/7008656.jpg',
+          author: 'Nicholas C. Zakas',
+          publisher: "'O'Reilly",
+          price: 23.49
+        },
+        {
+          book_name: 'The Definitive Guide',
+          img: 'http://d.gr-assets.com/books/1328834985l/7008656.jpg',
+          author: 'David Flanagan',
+          publisher: "'O'Reilly",
+          price: 27.49
         }
       ]
     };
